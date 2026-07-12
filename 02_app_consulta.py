00809d8f5c5bbe7c1376ec4c59b7a8a8d50887d6
JoséLizaAvalos <cs.joseliza@gmail.com>
2026-07-12 14:39:56 -0500

    Update 02_app_consulta.py
    Corrección de error con modulo pysqlite3

--- 02_app_consulta.py
+++ 02_app_consulta.py
@@ -2,9 +2,12 @@
 
 import streamlit as st
 import os
-__import__('pysqlite3')
 import sys
-sys.modules['sqlite3'] = sys.modules.pop['pysqlite3']
+try:
+    __import__('pysqlite3')
+    sys.modules['sqlite3'] = sys.modules['pysqlite3']
+except ImportError:
+    pass    
 import chromadb
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
